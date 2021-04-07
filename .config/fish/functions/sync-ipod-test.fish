ee15b896a12dbabd2a6f38a8118e243a99f6ecfb
Benjamin George Roberts <benjamin@bgroberts.id.au>
2021-04-07 23:31:24 +1000

    add ipod sync commands

---
+++ .config/fish/functions/sync-ipod-test.fish
@@ -0,0 +1,4 @@
+# Defined via `source`
+function sync-ipod-test --wraps=rsync\ -vrtnu\ --progress\ --exclude\ \'\*6ch\*/\'\ benjamin@freenas.int.bgroberts.id.au:/mnt/int_bgroberts_pool/Media/Audio/\ /Volumes/iPodClassic/ --description alias\ sync-ipod-test\ rsync\ -vrtnu\ --progress\ --exclude\ \'\*6ch\*/\'\ benjamin@freenas.int.bgroberts.id.au:/mnt/int_bgroberts_pool/Media/Audio/\ /Volumes/iPodClassic/
+  rsync -vrtnu --progress --exclude '*6ch*/' benjamin@freenas.int.bgroberts.id.au:/mnt/int_bgroberts_pool/Media/Audio/ /Volumes/iPodClassic/ $argv; 
+end
