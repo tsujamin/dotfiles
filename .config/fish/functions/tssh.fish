91095adfb06dfa923a9a7d020e5dec431140c599
Benjamin George Roberts <benjamin@bgroberts.id.au>
2021-05-18 09:44:28 +1000

    add tssh

---
+++ .config/fish/functions/tssh.fish
@@ -0,0 +1,4 @@
+# Defined in /Users/benjaminroberts/.config/fish/functions/tssh.fish @ line 2
+function tssh
+   ssh -t $argv 'if [ -n $(tmux list-session -F "#S" | grep tssh) ]; then tmux -CC new -A -s tssh; else tmux -CC attach -A -t tssh; fi'
+end
