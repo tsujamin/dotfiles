ea0fba0388b0077f35040ae0e180fde03ae4cd19
Benjamin George Roberts <benjamin@bgroberts.id.au>
2021-10-19 08:57:17 +1100

    fix tfissh

--- .config/fish/functions/tfissh.fish
+++ .config/fish/functions/tfissh.fish
@@ -1,9 +1,9 @@
-# Defined in /var/folders/07/jwb4g7cx1277dvy9tszxstp80000gn/T//fish.OndHCR/tfissh.fish @ line 2
+# Defined in /var/folders/07/jwb4g7cx1277dvy9tszxstp80000gn/T//fish.FFXZHj/tfissh.fish @ line 2
 function tfissh
 	ssh -t $argv '
 if tmux list-session -F "#S" | grep tssh
-	tmux -CC attach -A -s tssh
+	tmux -CC attach -t tssh
 else
-	tmux -CC new -A -t tssh
+	tmux -CC new -A -s tssh
 end'
 end
